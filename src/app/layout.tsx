aa3ceb75c897eb1a3ab3ac84edee44d3db0a9345
ErmesonOAlves <ermesonoliveira2000@gmail.com>
2026-03-13 18:06:40 -0300

    initial styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Jost } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const jost = Jost({
+  variable:'--font--jost',
+  subsets: ["latin"]
+})
+  
 
 export const metadata: Metadata = {
   title: "Adidas Store - Ecommerce",
@@ -25,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${jost.className} antialiased`}
       >
         {children}
       </body>
